374897f7bcedf05919db8920509c6a317a80473d
UasaLine <cop@ngs.ru>
2019-01-30 15:06:57 +0700

    - Tor

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -21,10 +21,11 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public static void main(String[] args) {
+        /*
         System.getProperties().put( "proxySet", "true" );
         System.getProperties().put( "socksProxyHost", "127.0.0.1" );
         System.getProperties().put( "socksProxyPort", "9150" );
-
+        */
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
